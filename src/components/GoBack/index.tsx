df25bb1954d91388718527de1bc07ce23b2b059f
SkyG0D <silvanosilvino@hotmail.com>
2022-04-25 22:35:01 -0300

    refactor: GoBack changed to link

--- src/components/GoBack/index.tsx
+++ src/components/GoBack/index.tsx
@@ -1,21 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import { Icon } from '@blueprintjs/core';
+import { Link } from 'react-router-dom';
+import { Colors, Icon } from '@blueprintjs/core';
 
 import styles from './styles.module.scss';
 
 export function GoBack(): JSX.Element {
-  const navigate = useNavigate();
-
-  function hanldeGoBack(): void {
-    navigate('/');
-  }
-
   return (
-    <Icon
-      className={styles.goBack}
-      icon="arrow-left"
-      size={20}
-      onClick={hanldeGoBack}
-    />
+    <Link to="/" style={{ color: Colors.BLACK }}>
+      <Icon className={styles.goBack} icon="arrow-left" size={20} />
+    </Link>
   );
 }
