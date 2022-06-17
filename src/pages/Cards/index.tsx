6f782b7c2fe87e9da421774efe6c2c350a131f40
SkyG0D <silvanosilvino@hotmail.com>
2022-06-17 01:48:27 -0300

    feat: add cardsFilter on Cards page

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,4 +1,5 @@
 import { H3 } from '@blueprintjs/core';
+import { useRef, useState } from 'react';
 
 import { Card } from '@/components/Card';
 
@@ -8,8 +9,14 @@ import { SomeLoading } from '@/components/SomeLoading';
 import { useFetchCards } from './useFetchCards';
 import styles from './styles.module.scss';
 
+type CardFilter = 'ALL' | 'WHITE' | 'BLACK';
+
 export function Cards(): JSX.Element {
   const { isLoading, cards } = useFetchCards();
+
+  const [cardsFilter, setCardsFilter] = useState<CardFilter>('ALL');
+
+  const selectRef = useRef<HTMLSelectElement>(null);
 
   const whites = cards.filter(({ type }) => type === 'WHITE');
   const blacks = cards.filter(({ type }) => type === 'BLACK');
@@ -22,30 +29,48 @@ export function Cards(): JSX.Element {
         <GoBack />
       </div>
 
-      <div className={styles.cardsContainer}>
-        <div className={`${styles.cards} ${styles.whites}`}>
-          <H3 className={`${styles.totalOfCards} ${styles.whites}`}>
-            Total de respostas: {whites.length}
-          </H3>
-          {whites.map(({ id, message, type }, index) => (
-            <Card
-              key={id}
-              message={message}
-              type={type}
-              animationType="auto"
-              animationDelay={`${index >= 20 ? 0 : index * 500}ms`}
-            />
-          ))}
-        </div>
+      <div className={`bp4-html-select ${styles.selectCards}`}>
+        <select
+          ref={selectRef}
+          onChange={(event) => setCardsFilter(event.target.value as CardFilter)}
+        >
+          <option value="ALL">Todas</option>
+          <option value="WHITE">Brancas</option>
+          <option value="BLACK">Pretas</option>
+        </select>
+        <span className="bp4-icon bp4-icon-double-caret-vertical" />
+      </div>
 
-        <div className={`${styles.cards} ${styles.blacks}`}>
-          <H3 className={`${styles.totalOfCards} ${styles.blacks}`}>
-            Total de perguntas: {blacks.length}
-          </H3>
-          {blacks.map(({ id, message, type }) => (
-            <Card key={id} message={message} type={type} />
-          ))}
-        </div>
+      {cardsFilter === 'BLACK' && <div className={styles.placeholderHeader} />}
+
+      <div className={styles.cardsContainer}>
+        {cardsFilter !== 'BLACK' && (
+          <div className={`${styles.cards} ${styles.whites}`}>
+            <H3 className={`${styles.totalOfCards} ${styles.whites}`}>
+              Total de respostas: {whites.length}
+            </H3>
+            {whites.map(({ id, message, type }, index) => (
+              <Card
+                key={id}
+                message={message}
+                type={type}
+                animationType="auto"
+                animationDelay={`${index >= 20 ? 0 : index * 500}ms`}
+              />
+            ))}
+          </div>
+        )}
+
+        {cardsFilter !== 'WHITE' && (
+          <div className={`${styles.cards} ${styles.blacks}`}>
+            <H3 className={`${styles.totalOfCards} ${styles.blacks}`}>
+              Total de perguntas: {blacks.length}
+            </H3>
+            {blacks.map(({ id, message, type }) => (
+              <Card key={id} message={message} type={type} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
