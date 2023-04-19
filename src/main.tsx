8e1b702b72fe9072be094ca17542bbfb8850ab9c
SkyG0D <silvanosilvino@hotmail.com>
2023-04-18 22:40:15 -0300

    refactor: try to fix google ads

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <BrowserRouter>
