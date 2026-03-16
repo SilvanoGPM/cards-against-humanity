68e9dc9ac6d1ef614801a7dc317b29fd7fab9217
Silvano Marques <silvanosilvino@hotmail.com>
2026-03-16 11:17:03 -0300

    ci: action para finalizar partidas atualizado

--- scripts/finish-matches.js
+++ scripts/finish-matches.js
@@ -1,50 +1,30 @@
 /* eslint-disable */
 
-const { initializeApp } = require('firebase/app');
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
-const {
-  getFirestore,
-  collection,
-  query,
-  where,
-  getDocs,
-  updateDoc,
-  doc,
-} = require('firebase/firestore');
+const app = initializeApp({
+  credential: cert(require('./serviceAccountKey.json')),
+});
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCJh45lbjhCek0BP30K7YvSK-hG-8SeWa0',
-  authDomain: 'cards-against-humanity-16605.firebaseapp.com',
-  projectId: 'cards-against-humanity-16605',
-  storageBucket: 'cards-against-humanity-16605.appspot.com',
-  messagingSenderId: '1094359847041',
-  appId: '1:1094359847041:web:a8ecc0bd327d85ee5f3d80',
-  measurementId: 'G-2GNVB3MBHM',
-};
-
-const app = initializeApp(firebaseConfig);
-
-const matchesCollection = collection(getFirestore(app), 'matches');
-
-const q = query(matchesCollection, where('status', '!=', 'FINISHED'));
+const db = getFirestore(app);
 
 const finishMatches = async () => {
   try {
-    const querySnapshot = await getDocs(q);
+    const snapshot = await db
+      .collection('matches')
+      .where('status', '!=', 'FINISHED')
+      .get();
 
-    querySnapshot.forEach(async (doc) => {
-      try {
-        await updateDoc(doc.ref, {
-          status: 'FINISHED',
-        });
+    const batch = db.batch();
 
-        console.log(`Partida ${doc.id} finalizada com sucesso.`);
-      } catch (error) {
-        console.error(`Erro ao finalizar partida ${doc.id}:`, error);
-      }
+    snapshot.forEach((doc) => {
+      batch.update(doc.ref, { status: 'FINISHED' });
     });
 
-    console.log(`${querySnapshot.size} Partida(s) finalizadas com sucesso.`);
+    await batch.commit();
+
+    console.log(`${snapshot.size} Partida(s) finalizadas com sucesso.`);
   } catch (error) {
     console.error('Erro ao finalizar partidas:', error);
   }
